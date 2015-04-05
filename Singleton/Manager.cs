e025453c58488884ac65126af0dc982a8d366723
s.degambe@gmail.com <Sebad#21>
2015-04-05 19:31:36 +0200

    .

---
+++ Singleton/Manager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+	public interface IManager
+	{
+		string Test();
+
+	}
+	class Manager : IManager
+	{
+		private static Manager _manage;
+
+		public static Func<IManager> Manage = () => _manage ?? (_manage = new Manager());
+		public string Test()
+		{
+			return "test";
+		}
+	}
+}
